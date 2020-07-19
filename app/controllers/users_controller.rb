4484c3cd9a1835856e6344384d1c86cb69a14200
Hien Tran <dinhhiencit@gmail.com>
2020-07-19 22:42:04 +0700

    Implement Login/Logout/Register

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  def login
+    @user = User.find_by(email: params[:email])
+
+    if @user
+      if @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+      else
+        flash[:alert] = {:password => ["Password is not correct!"]}
+      end
+    else
+      register(params[:email], params[:password])
+      session[:user_id] = @user.id
+    end
+
+    redirect_to root_path
+  end
+
+  def logout
+    session[:user_id] = nil
+    redirect_to root_url
+  end
+
+  private
+  def register(email, password)
+    @user = User.new(email: email, password: password)
+    if @user.valid?
+      @user.save
+    else
+      flash[:alert] = @user.errors
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 
   root 'home#index'
+
+  post 'login', to: 'users#login'
+  get 'logout', to: 'users#logout'
 end
