7ab4caa7cacaec9cc33ade33b8bd06482e3d40da
Hien Tran <dinhhiencit@gmail.com>
2020-07-20 13:32:43 +0700

    Add validating email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Email is not valid!"}
   validates :password, presence: true, length: { minimum: 6, message: "Password is too short (minimum is 6 characters)!" }
 
   has_many :movies
